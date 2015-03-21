e4266ca6c584628f842b13671e1d69f4b7b45355
Guillaume Maiano <Guillaume.Maiano@locuz.nl>
2015-03-21 14:57:24 +0100

    Demonstrates method delegation in Ruby

---
+++ Ruby/DelegatorUsage.rb
@@ -0,0 +1,43 @@
+# In Ruby, SimpleDelegator is used to delegate unknown methods to an object
+# method_missing is called when a method is... missing.
+require 'delegate'
+
+class MyDelegatedClass < SimpleDelegator
+
+  def initialize(object)
+    super(object)
+  end
+
+end
+
+# This class delegates a set of methods to another class, and whines for others
+class MyOtherClass
+  METHODS_TO_DELEGATE = [ :example_method1, :example_method2]
+
+  def initialize(delegate)
+    @delegate = delegate
+  end
+
+  def method_missing(method_name, *args)
+    if METHODS_TO_DELEGATE.include?(method_name)
+      @delegate.send(method_name, *args)
+    else
+      logger.warn "Y U SO STUPID G? No #{method_name} for #{args} here..."
+      super
+    end 
+  end
+
+end
+
+# This class responds to the delegator
+class MyDelegateClass
+
+  def example_method1
+    puts "hi"
+  end
+
+  def example_method2
+    puts "Also works!"
+  end
+
+end
