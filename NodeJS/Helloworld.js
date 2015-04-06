cb5556850d1b3eb479d4b2c219ed8e9f7ec45097
Guillaume.Maiano@astutemonkey.com <Guillaume.Maiano@astutemonkey.com>
2015-04-06 02:46:57 +0200

    Added some basic settings and error detection. Learning NodeJS is fun for now.

--- NodeJS/Helloworld.js
+++ NodeJS/Helloworld.js
@@ -1,12 +1,41 @@
 var http = require("http");
+var fs = require('fs');
+
 
 var port = 9977;
-var server = http.createServer(
-    function(request, response){
-        response.writeHead(200); // HTTP status code in header (200 for OK)
-        response.write("<!DOCTYPE html><html><body>nothing</body></html>");// response body
-        response.end(); // close the connection
+var application = function(){
+  var settings = JSON.parse(contentsOfSettings);
+  var server = http.createServer(
+      function(request, response){
+          response.writeHead(200); // HTTP status code in header (200 for OK)
+          response.write("<!DOCTYPE html><html><body>nothing</body></html>");// response body
+          response.end(); // close the connection
+      }
+  );
+  server.listen(port); // listen for connections on a port I'm not using otherwise
+  console.log("Listening on port "+port);
+}
+
+try {
+  var contentsOfSettings = fs.readFileSync('settings.json'); // Synchronous because you need the settings to execute properly
+  try {
+    try {
+      application();
     }
-);
-server.listen(port); // listen for connections on a port I'm not using otherwise
-console.log("Listening on port "+port);
+    catch(e_net) {
+      console.log("Network issue");
+    }
+  }
+  catch(e_parse) {
+    console.log("Settings file couldn't be parsed correctly");
+  }
+}
+catch(e_file) {
+  if (e_file.code === 'ENOENT'){
+    console.log("Settings file not found");
+    return;
+  }
+  else {
+    console.log(e_file);
+  }
+}
