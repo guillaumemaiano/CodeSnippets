c1ea49660609a036c8ce3cb5c324e9e86727ed9d
Guillaume.Maiano@astutemonkey.com <Guillaume.Maiano@astutemonkey.com>
2015-04-07 20:09:27 +0200

    Adds a slightly different syntax

--- NodeJS/Helloworld.js
+++ NodeJS/Helloworld.js
@@ -4,9 +4,29 @@ var fs = require('fs');
 
 var port = 9988;
 var duration = 3000;
-var application = function(){
+
+var setupImprovedServer = function(server) {
+  server.on('request', function(request, response) {
+    response.writeHead(200);
+    response.write("<!DOCTYPE html><html><body>nothing</body></html>");
+    response.end();
+  });
+};
+
+var demonstrateAnotherSyntaxForServerDeclaration = function(){
+  // the next exemple improves the server declaration to be more readable
+  var server2 = http.createServer();
+  setupImprovedServer(server2);
+  server2.listen(port); // listen for connections on a port I'm not using otherwise
+  console.log("Listening on port "+port+" for the next "+duration/1000+" seconds.");
+  setTimeout(function() {
+    server2.close();
+  }, duration);
+}
+
+var application = function() {
   var server = http.createServer(
-      function(request, response){
+      function(request, response) {
           response.writeHead(200); // HTTP status code in header (200 for OK)
           response.write("<!DOCTYPE html><html><body>nothing</body></html>");// response body
           response.end(); // close the connection
@@ -14,9 +34,10 @@ var application = function(){
   );
   server.listen(port); // listen for connections on a port I'm not using otherwise
   console.log("Listening on port "+port+" for the next "+duration/1000+" seconds.");
-  setTimeout(function(){
+  setTimeout(function() {
     server.close();
-  },duration);
+    demonstrateAnotherSyntaxForServerDeclaration();
+  }, duration);
 
   process.on('SIGTERM', function () { // gracefully deals with server being killed (SIGKILL cannot be handled)
     console.log("Shutting down");
@@ -24,7 +45,10 @@ var application = function(){
       process.exit(0);
     });
   });
+   
 }
+
+
 
 try {
   var contentsOfSettings = fs.readFileSync('settings.json'); // Synchronous because you need the settings to execute properly
