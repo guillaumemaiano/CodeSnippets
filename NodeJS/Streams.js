5f5e3854412273bcb8b094bba939a1afac69ccad
Guillaume.Maiano@astutemonkey.com <Guillaume.Maiano@astutemonkey.com>
2015-04-10 01:49:12 +0200

    Fixes example, shows an example of pipe

--- NodeJS/Streams.js
+++ NodeJS/Streams.js
@@ -7,15 +7,42 @@ var http = require('http');
 
 var server = http.createServer( function( request, response) {
     response.writeHead(200);
+    request.on('readable', function() {
+      var chunk = null;
+      while ( null !== ( chunk = request.read()))
+        {
+            console.log("Chunk: "+chunk.toString());
+        }
+});
+
+    request.on('end', function() {
+            response.end();
+    });            
+});
+
+server.listen(7777);
+
+
+
+var server1 = http.createServer( function( request, response) {
+    response.writeHead(200);
     request.on('readable', function(){
       var chunk = null;
       while ( null !== ( chunk = request.read()))
         {
-            request.on('end',function(){
-                response.end();
-            });            
+            response.write(chunk);
         }
-});	
 });
 
-server.listen(7777);
+    request.on('end', function() {
+            response.end();
+    });            
+});
+
+server1.listen(8888);
+
+
+http.createServer( function( request, response) {
+    response.writeHead(200);
+    request.pipe(response);
+}).listen(9999);
