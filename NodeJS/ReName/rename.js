3ca4745985aeec5cb9a300e7696fe73639d211c2
Guillaume.Maiano@astutemonkey.com <Guillaume.Maiano@astutemonkey.com>
2015-05-01 13:08:17 +0200

    Parses items

--- NodeJS/ReName/rename.js
+++ NodeJS/ReName/rename.js
@@ -7,19 +7,24 @@ var isEven = function(someNumber){
 
 // argument processing
 process.argv.every(function (val, index, array) {
-    var warningDisplayed = false;
+    var action = "";
+    var actions = ["dout", "din", "prefix"];
     // Index 0 is "node"
     // Index 1 is the program's filename
     // even indexed values are arguments values
     // odd indexed values are argument parameters 
     if ( 0 != index && isEven(index) ) {
         if ( val.lastIndexOf("-", 0) === 0 ) {
-            console.log(val);
-            console.log("OK!");
+            actions.forEach(function(action_cursor, index, array){
+                if (val==="-"+action_cursor) {
+                    action = action_cursor;
+                }
+            });
         } else {
             console.log("Arguments: -din -dout -prefix");
             return false;
-        } 
+        }
+       console.log(action); 
      } 
     return true;
 });
