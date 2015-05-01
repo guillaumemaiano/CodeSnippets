1b7ede129ed02aa92872bfc4c7eb5c6003a0121e
Guillaume.Maiano@astutemonkey.com <Guillaume.Maiano@astutemonkey.com>
2015-05-01 13:34:49 +0200

    This adds code, but variables are not changed properly yet.

--- NodeJS/ReName/rename.js
+++ NodeJS/ReName/rename.js
@@ -1,12 +1,36 @@
 // imports
 var fs = require('fs');
+
 // helper methods
 var isEven = function(someNumber){
         return (someNumber%2 == 0) ? true : false;
 };
 
+var makeDateString = function() {
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+        dd='0'+dd
+} 
+
+if(mm<10) {
+        mm='0'+mm
+} 
+
+today = yyyy+"_"+mm+"_"+dd; // UN standard
+return today;
+}
+
+// variables
+var directory_out = ".";
+var directory_in = ".";
+var prefix = makeDateString();
+
 // argument processing
-process.argv.every(function (val, index, array) {
+process.argv.every( function(val, index, array) {
     var action = "";
     var actions = ["dout", "din", "prefix"];
     // Index 0 is "node"
@@ -26,5 +50,24 @@ process.argv.every(function (val, index, array) {
         }
        console.log(action); 
      } 
+    else {
+        switch (action) {
+            case "dout":
+               directory_out = val;
+               break;
+            case "din": 
+               directory_in = val;
+               break;
+            case "prefix": 
+               prefix = val;
+               break;
+            default:;
+        }
+    }
     return true;
 });
+
+    console.log("Settings:\nInput directory: "+directory_in);
+    console.log("Output directory: "+directory_out);
+    console.log("Prefix: "+prefix);
+
